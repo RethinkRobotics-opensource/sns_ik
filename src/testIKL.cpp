af1f50cecddb28b751827df1f0f2bf99e2403e4c
Ian McMahon <imcmahon@rethinkrobotics.com>
2016-02-09 12:47:09 -0500

    Initial commit for SNS IK

---
+++ src/testIKL.cpp
@@ -0,0 +1,29 @@
+/* test for the IK library*/
+
+#include <IKL/IKL.hpp>
+
+#include <Eigen/Dense>
+#include <iostream>
+
+using namespace Eigen;
+using namespace IKL_;
+
+int main(int argc, char** argv) {
+  
+  IKL inverseKinematic;
+  StackOfTasks sot;
+  Task task;
+  
+  VectorD jointVelocity;
+  
+  task.jacobian = MatrixD::Random(3,7);
+  task.desired =  MatrixD::Random(3,1);
+
+  sot.push_back(task);
+  
+  inverseKinematic.getJointVelocity(&jointVelocity,&sot);
+  
+  cout << "result :"<< endl << jointVelocity << endl;
+  
+
+}
