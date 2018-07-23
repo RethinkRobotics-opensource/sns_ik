e3a738d0b4082bee0b5059813cea639f474fa67e
Andy Park <apark@rethinkrobotics.com>
2018-07-23 11:01:02 -0400

    Refactoring base vel/acc IK codes with a base class (#95)
    * Create a base SNS IK class
    
    - all the common functions are moved to sns base class
    - updated velocity and acceleration IK codes so that
      they use the functions in the base class
    - Moved Eigen-based formatting to base class.
    - Remove duplicate code in Matlab functions
    - In velocity/acceleration IK with configuration task,
      we now reuse the base IK function for primary task.
    
    (cherry picked from commit b4b4d08dc23c1d581a71cf0bd67ec3f554ffc8f9)

--- sns_ik_lib/matlab/snsIk_acc_rr_cs.m
+++ sns_ik_lib/matlab/snsIk_acc_rr_cs.m
@@ -47,64 +47,10 @@ function [ddq, s, sCS, exitCode] = snsIk_acc_rr_cs(ddqLow, ddqUpp, ddxGoal, ddqC
 % TODO: input validation
 
 %% compute the solution for the primary task
-[nTask, nJnt] = size(J);
-W = eye(nJnt);
-ddqNull = zeros(nJnt, 1);
-s = 1.0;
-sStar = 0.0;
-exitCode = 1;
+nJnt = size(J,2);
 tol = 1e-6;
-limitExceeded = true;
-while limitExceeded == true
-    limitExceeded = false;
-    ddq = ddqNull + pinv(J*W) * (ddxGoal -dJdq - J*ddqNull);
 
-    if any(ddq < (ddqLow - tol)) || any(ddq > (ddqUpp + tol))
-        limitExceeded = true;
-    end
-    a = pinv(J*W) * ddxGoal;
-    b = ddq - a;
-
-    marginL = ddqLow - b;
-    marginU = ddqUpp - b;
-    sMax = zeros(nJnt, 1);
-
-    for i=1:nJnt
-        if W(i,i) == 0
-            sMax(i) = inf;
-        else
-            sMax(i) = FindScaleFactor(marginL(i), marginU(i), a(i));
-            % if scale factor is 1 but joint limit is violated,
-            % set a scale factor to a small number so that the
-            % corresponding joint will be saturated
-            if(sMax(i) == 1 && (ddq(i) < (ddqLow(i) - tol) || ddq(i) > (ddqUpp(i) + tol)))
-                sMax(i) = 1e-3;
-            end
-        end
-    end
-
-    [~, jntIdx] = min(sMax);
-    taskScale = sMax(jntIdx);
-    % if the current best scaled solution violates the limit,
-    % update sStar to the latest scale factor
-    ddqTmp = ddqNull + pinv(J*W)*(sStar * ddxGoal - dJdq - J * ddqNull);
-    if ((taskScale > sStar) || (any(ddqTmp > ddqUpp + tol) || any(ddqTmp < ddqLow - tol)))
-        sStar = taskScale;
-        Wstar = W;
-        ddqNullStar = ddqNull;
-    end
-
-    W(jntIdx, jntIdx) = 0;
-    ddqNull(jntIdx) = min(max(ddqLow(jntIdx), ddq(jntIdx)), ddqUpp(jntIdx));
-
-    if rank(J*W) < nTask
-        s = sStar;
-        W = Wstar;
-        ddqNull = ddqNullStar;
-        ddq = ddqNull + pinv(J*W)*(s * ddxGoal - dJdq - J * ddqNull);
-        limitExceeded = false;
-    end
-end
+[ddq, s, exitCode] = snsIk_acc_rr(ddqLow, ddqUpp, ddxGoal, J, dJdq);
 
 %% compute the solution for the secondary task
 
